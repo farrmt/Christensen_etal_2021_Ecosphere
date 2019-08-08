670e5b9acaf33c3c1dc230a2981882ea34329a5f
Matthew Farr <matthewtfarr@gmail.com>
2019-08-08 16:54:02 -0400

    Comment simulation

--- Simulation.R
+++ Simulation.R
@@ -1,21 +1,39 @@
+#----------------------------------------------------#
+#----Simulation of sensitivity analysis for----------#
+#----N-mixture and Distance sampling models----------#
+#----Created by: Matthew Farr, Sonja Christensen,----#
+#----and David Williams------------------------------#
+#----------------------------------------------------#
+
+#-----------#
 #-Libraries-#
+#-----------#
+
 library(dplyr)
 library(jagsUI)
 
+#-----------#
 #-Load data-#
+#-----------#
 
 data <- list()
 data[[1]] <- read.csv("Aerial2014xy.csv", header = TRUE)
 data[[2]] <- read.csv("Aerial2016xy.csv", header = TRUE)
 data[[3]] <- read.csv("StartPoint.csv")
 
+#------------#
 #-Data clean-#
+#------------#
+
+#Convert distance class and remove unknown classes
 data[[2]] <- data[[2]] %>%
   mutate(Distance_C = recode(Distance_C, "A/B" = "A")) %>%
   filter(Distance_C != "") %>%
   droplevels()
 
-#-Extract data-#
+#---------#
+#-Indices-#
+#---------#
 
 #Number of years
 nyrs <- 2
@@ -25,38 +43,59 @@ nobs <- NULL
 nobs[1] <- dim(data[[1]])[1] #2014
 nobs[2] <- dim(data[[2]])[1] #2016
 
-#-Transect breaks-#
-
+#Generate random starting points
 start.point <- floor(runif(1, 0, 21000))  
 
+#Unit size in meters
 unit.size <- c(1000, 2000, 4000, 5000, 6000, 8000, 10000)
 
+#--------#
+#-Output-#
+#--------#
+
+#List of models for each unit size
 S <- list()
 
+#Output of each model (dimensions: iteration [1/script] x parameter [17] x unit size [7])
 Out <- array(NA, dim = c(1,17,7))
-dimnames(Out)[[2]] <- c("N.ds.14.mu", "N.ds.16.mu", "N.nmix.14.mu", "N.nmix.16.mu",
-                        "pi.mu", "p.mu", "phi.ds.mu", "phi.nmix.mu",
-                        "N.ds.14.sd", "N.ds.16.sd", "N.nmix.14.sd", "N.nmix.16.sd",
-                        "pi.sd", "p.sd", "phi.ds.sd", "phi.nmix.sd",
-                        "Rhat")
+dimnames(Out)[[2]] <- c("N.ds.14.mu", #Mean abundance; distance sampling; 2014
+                        "N.ds.16.mu", #Mean abundance; distance sampling; 2016
+                        "N.nmix.14.mu", #Mean abundance; N-mixture; 2014
+                        "N.nmix.16.mu", #Mean abundance; N-mixture; 2016
+                        "pi.mu", #Mean detection probability; distance sampling
+                        "p.mu", #Mean detection probability; N-mixture
+                        "phi.ds.mu", #Mean availability probability; distance sampling
+                        "phi.nmix.mu", #Mean availability probability; N-mixture
+                        "N.ds.14.sd", #SD abundance; distance sampling; 2014
+                        "N.ds.16.sd", #SD abundance; distance sampling; 2016
+                        "N.nmix.14.sd", #SD abundance; N-mixture; 2014
+                        "N.nmix.16.sd", #SD abundance; N-mixture; 2016
+                        "pi.sd", #SD detection probability; distance sampling
+                        "p.sd", #SD detection probability; N-mixture
+                        "phi.ds.sd", #SD availability probability; distance sampling
+                        "phi.nmix.sd", #SD availability probability; N-mixture
+                        "Rhat") #Gelman-Rubin diagnostic
 
-start.time <- Sys.time()
+#---------------------#
+#-Simulate unit sizes-#
+#---------------------#
+
+start.time <- Sys.time() #begin run time
 for(u in 1:7){
   
-  n.unit <- floor(21000/unit.size[u])
-  len.units <- n.unit*unit.size[u]
-  extra <- 21000-len.units
-  spacing <- floor(extra/n.unit)
+  n.unit <- floor(21000/unit.size[u]) #Number of units
+  len.units <- n.unit*unit.size[u] #Total length of units
+  extra <- 21000-len.units #Left over transect
+  spacing <- floor(extra/n.unit) #Spacing between units
+  breaks <- NULL #Unit breaks
+  breaks[1] <- start.point #First unit break
+  site <- list(rep(NA, nobs[1]), rep(NA, nobs[2])) #Site ID
   
-  breaks <- NULL
-  breaks[1] <- start.point
-  
-  site <- list(rep(NA, nobs[1]), rep(NA, nobs[2]))
-  
-  for(i in 2:(n.unit+1)){
-    if((breaks[i-1] + unit.size[u] + spacing) < 21000){
-      breaks[i] <- breaks[i-1] + unit.size[u] + spacing
-      for(t in 1:nyrs){
+  for(i in 2:(n.unit+1)){ #Generate unit break points
+    if((breaks[i-1] + unit.size[u] + spacing) < 21000){ #Execute if unit doesn't wrap transect
+      breaks[i] <- breaks[i-1] + unit.size[u] + spacing #Break point
+      #Extract observations within unit
+      for(t in 1:nyrs){ 
         for(j in 1:nobs[t]){
           start.coord <- as.numeric(data[[3]] %>% 
                                       filter(Transect == data[[t]]$Transect[j] & 
@@ -69,8 +108,9 @@ for(u in 1:7){
           }#end if
         }#end j
       }#end t
-    }else{
-      breaks[i] <- unit.size[u] - (21000 - (breaks[i-1] + spacing))
+    }else{ #Execute if unit does wrap transect
+      breaks[i] <- unit.size[u] - (21000 - (breaks[i-1] + spacing)) #Break point
+      #Extract observations within unit
       for(t in 1:nyrs){
         for(j in 1:nobs[t]){
           start.coord <- as.numeric(data[[3]] %>% 
@@ -88,32 +128,33 @@ for(u in 1:7){
       }#end t
     }#end if/else
   }#end i
+  
   obs <- list()
-  obs[[1]] <- which(!is.na(site[[1]]))
-  obs[[2]] <- which(!is.na(site[[2]]))
-  site <- c(site[[1]], site[[2]])
-  extract <- which(!is.na(site))
-  site <- site[extract]
-  year <- c(rep(1, nobs[1]), rep(2, nobs[2]))
-  year <- year[extract]
-  day <- c(data[[1]]$Survey, data[[2]]$Survey)
-  day <- day[extract]
-  rep <- c(as.numeric(data[[1]]$Heading_Di), as.numeric(data[[2]]$Heading_Di))
-  rep <- rep[extract]
-  dclass <- c(as.numeric(data[[1]]$Distance_C), as.numeric(data[[2]]$Distance_C))
-  dclass <- dclass[extract]
-  mdpt <- c(50, 150, 275, 425, 750)
-  nK <- 5
-  v <- c(100, 200, 250, 250, 500)
-  B <- 1000
-  ntransects <- 5
-  nsites <- n.unit * ntransects
-  ndays <- c(4,3)
-  nreps <- max(rep)
-  groups <- array(0, dim = c(nyrs, nsites, max(ndays), nreps))
-  counts <- array(0, dim = c(nyrs, nsites, max(ndays), nreps))
-  nobs[1] <- length(obs[[1]])
-  nobs[2] <- length(obs[[2]])
+  obs[[1]] <- which(!is.na(site[[1]])) #Observation ID 2014
+  obs[[2]] <- which(!is.na(site[[2]])) #Observation ID 2016
+  site <- c(site[[1]], site[[2]]) #Site ID
+  extract <- which(!is.na(site)) #Observation not in units
+  site <- site[extract] #Remove observations not in units
+  year <- c(rep(1, nobs[1]), rep(2, nobs[2])) #Year ID
+  year <- year[extract] #Remove observations not in units
+  day <- c(data[[1]]$Survey, data[[2]]$Survey) #Day ID
+  day <- day[extract] #Remove observations not in units
+  rep <- c(as.numeric(data[[1]]$Heading_Di), as.numeric(data[[2]]$Heading_Di)) #Replicate ID
+  rep <- rep[extract] #Remove observations not in units
+  dclass <- c(as.numeric(data[[1]]$Distance_C), as.numeric(data[[2]]$Distance_C)) #Distance class
+  dclass <- dclass[extract] #Remove observations not in units
+  mdpt <- c(50, 150, 275, 425, 750) #Midpoint of each distance class
+  nK <- 5 #Number of distance classes
+  v <- c(100, 200, 250, 250, 500) #Width of each distance class
+  B <- 1000 #Transect half-width
+  ntransects <- 5 #Number of transects
+  nsites <- n.unit * ntransects #Number of sites
+  ndays <- c(4,3) #Number of days
+  nreps <- max(rep) #Number of replicates
+  groups <- array(0, dim = c(nyrs, nsites, max(ndays), nreps)) #Number of groups
+  counts <- array(0, dim = c(nyrs, nsites, max(ndays), nreps)) #Counts 
+  nobs[1] <- length(obs[[1]]) #Number of observations 2014
+  nobs[2] <- length(obs[[2]]) #Number of observations 2016
   for(i in obs[[1]]){
     groups[year[i],site[i],day[i],rep[i]] <- groups[year[i],site[i],day[i],rep[i]] + 1
     counts[year[i],site[i],day[i],rep[i]] <- data[[1]]$Group_Size[i] + counts[year[i],site[i],day[i],rep[i]]
@@ -122,14 +163,21 @@ for(u in 1:7){
     groups[year[i + nobs[1]],site[i + nobs[1]],day[i + nobs[1]],rep[i + nobs[1]]] <- groups[year[i + nobs[1]],site[i + nobs[1]],day[i + nobs[1]],rep[i + nobs[1]]] + 1
     counts[year[i + nobs[1]],site[i + nobs[1]],day[i + nobs[1]],rep[i + nobs[1]]] <- data[[2]]$Group_Size[i] + counts[year[i + nobs[1]],site[i + nobs[1]],day[i + nobs[1]],rep[i + nobs[1]]]
   }
-  gs <- c(data[[1]]$Group_Size, data[[2]]$Group_Size)
-  gs <- gs[extract]
+  gs <- c(data[[1]]$Group_Size, data[[2]]$Group_Size) #Group size
+  gs <- gs[extract] #Remove observations not in units
+  sites <- which(seq(1, n.unit*5) %in% site) #Nested index for sites with groups > 0
   
-  sites <- which(seq(1, n.unit*5) %in% site)
+  #--------------#
+  #-Compile data-#
+  #--------------#
   
-  bugs.data <- list(Y = groups, X = groups, gs = gs, nobs = nobs[1] + nobs[2], nyrs = nyrs, nsites = nsites, sites = sites,
+  jags.data <- list(Y = groups, X = groups, gs = gs, nobs = nobs[1] + nobs[2], nyrs = nyrs, nsites = nsites, sites = sites,
                     ndays = ndays, nreps = nreps, nK = nK, mdpt = mdpt, dclass = dclass, v = v, B = B,
                     year = year, site = site, day = day, rep = rep)
+  
+  #----------------#
+  #-Initial values-#
+  #----------------#
   
   Nst <- round(apply(groups, c(1,2,3), max))
   Nst[2,1:nsites,4] <- NA
@@ -140,9 +188,16 @@ for(u in 1:7){
                            p = runif(1, 0.4, 0.5), alphax = runif(nyrs, 0, 2),
                            alphay = runif(nyrs, 0, 2), rx = runif(1, 0, 2),
                            ry = runif(1, 0, 2), rg = runif(1, 0, 2))}
+  #------------#
+  #-Parameters-#
+  #------------#
   
   params <- c("mean.Abundx", "mean.Abundy", "alphax", "alphay", "rx", "ry", 
               "p", "gamma", "pi", "phix", "phiy", "rg", "beta")
+  
+  #---------------#
+  #-MCMC settings-#
+  #---------------#
   
   nb <- 80000
   ni <- 120000
@@ -150,11 +205,19 @@ for(u in 1:7){
   nc <- 3
   na <- 1000
   
+  #-----------#
+  #-Run model-#
+  #-----------#
+  
   S[[u]] <- jagsUI(bugs.data, inits, params, model = "DS_Nmix_Emm.txt",
                    n.burnin = nb, n.iter = ni, n.thin = nt, 
                    n.chains = nc, n.adapt = na, parallel = TRUE)
   
   print(u)
+  
+  #----------------#
+  #-Extract output-#
+  #----------------#
   
   tryCatch({Out[1,1,u] <- S[[u]]$mean$mean.Abundx[1]}, error = function(e){})
   tryCatch({Out[1,2,u] <- S[[u]]$mean$mean.Abundx[2]}, error = function(e){})
@@ -176,10 +239,13 @@ for(u in 1:7){
   
 }#end u
 
-end.time <- Sys.time()
-Time <- end.time - start.time
+end.time <- Sys.time() #end run time
+Time <- end.time - start.time #run time
 
-#-Save HPCC output-#
+#-------------------#
+#-Computing cluster-#
+#-------------------#
+
 ID <- gsub(" ","_",Sys.time())
 ID <- gsub(":", "-", ID)
 output <- list(Out, Time)
